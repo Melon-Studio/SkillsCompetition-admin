21b3fbd18ae4c3308b0920a6f14558fb8c10f70c
XiaoFans <xiaofan6@foxmail.com>
2023-09-19 15:42:32 +0800

    StartLogo

--- src/main/java/top/dooper/SkillsCompetitionAdminApplication.java
+++ src/main/java/top/dooper/SkillsCompetitionAdminApplication.java
@@ -3,12 +3,14 @@ package top.dooper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import top.dooper.common.StartLogo;
 
 
 @SpringBootApplication
 @MapperScan("top.dooper.sys.mapper")
 public class SkillsCompetitionAdminApplication {
     public static void main(String[] args) {
+        StartLogo.init();
         SpringApplication.run(SkillsCompetitionAdminApplication.class, args);
     }
 }
---
+++ src/main/java/top/dooper/common/StartLogo.java
@@ -0,0 +1,21 @@
+package top.dooper.common;
+
+public class StartLogo {
+    public static void init(){
+        System.out.println("   _____ _    _ _ _   ");
+        System.out.println("  /  ___| |  (_) | |      ");
+        System.out.println("  \\ `--.| | ___| | |___        ");
+        System.out.println("   `--. \\ |/ / | | / __|        ");
+        System.out.println("  /\\__/ /   <| | | \\__ \\        ");
+        System.out.println("  \\____/|_|\\_\\_|_|_|___/      ");
+        System.out.println("   _____                            _   _ _   _             ");
+        System.out.println("  /  __ \\                          | | (_) | (_)            ");
+        System.out.println("  | /  \\/ ___  _ __ ___  _ __   ___| |_ _| |_ _  ___  _ __  ");
+        System.out.println("  | |    / _ \\| '_ ` _ \\| '_ \\ / _ \\ __| | __| |/ _ \\| '_ \\ ");
+        System.out.println("  | \\__/\\ (_) | | | | | | |_) |  __/ |_| | |_| | (_) | | | |");
+        System.out.println("   \\____/\\___/|_| |_| |_| .__/ \\___|\\__|_|\\__|_|\\___/|_| |_|");
+        System.out.println("                        | |                                 ");
+        System.out.println("                        |_|                                 ");
+        System.out.println("Skills Competition V2.1 启动！");
+    }
+}
